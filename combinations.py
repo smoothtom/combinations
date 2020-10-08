c4171aac187301b034b265c7cca3183712b2244c
smoothtom <t-lauth@web.de>
2020-10-08 15:03:40 +0200

    changes

--- combinations.py
+++ combinations.py
@@ -5,52 +5,54 @@ import os
 import time 
 import datetime 
 
+def intro():
+    print("\n")
+    print("*" * 41)
+    print("** Welcome to my Combination Programm! **")
+    print("*" * 41)
 
-print("\n")
-print("*" * 41)
-print("** Welcome to my Combination Programm! **")
-print("*" * 41)
+def main():
+    print("\n")
+    s = input("Please Enter something for different combinations: ")
+    print("\n")
+    t = list(itertools.permutations(s,len(s)))
 
-print("\n")
-s = input("Please Enter something for different combinations: ")
-print("\n")
-t = list(itertools.permutations(s,len(s)))
+    print("loading the combinations...")
+    time.sleep(2)
+    x = 0
 
-print("loading the combinations...")
-time.sleep(2)
-x = 0
+    for i in range (0,len(t)):
+        x += 1
+        if len(s) <= 3:
+            print("".join(t[i]),"-->", x )
+            time.sleep(0.04)
+        elif len(s) == 4:
+            print("".join(t[i]),"-->", x )
+            time.sleep(0.03)
+        elif len(s) == 5:
+            print("".join(t[i]),"-->", x )
+            time.sleep(0.02)
+        elif len(s) == 6:
+            print("".join(t[i]),"-->", x )
+            time.sleep(0.01)
+        elif len(s) == 7:
+            print("".join(t[i]),"-->", x )
+            time.sleep(0.001)
+        elif len(s) > 7:
+            print("".join(t[i]),"-->", x )
 
-for i in range (0,len(t)):
-    x += 1
-    if len(s) == 3:
-        print("".join(t[i]),"-->", x )
-        time.sleep(0.04)
-    elif len(s) == 4:
-        print("".join(t[i]),"-->", x )
-        time.sleep(0.03)
-    elif len(s) == 5:
-        print("".join(t[i]),"-->", x )
-        time.sleep(0.02)
-    elif len(s) == 6:
-        print("".join(t[i]),"-->", x )
-        time.sleep(0.01)
-    elif len(s) == 7:
-        print("".join(t[i]),"-->", x )
-        time.sleep(0.001)
-    elif len(s) > 7:
-        print("".join(t[i]),"-->", x )
+    print("\n")
+    i = input("Do you want to save all these combinations ? Please Enter yes or no: ")
+    if i == "yes":
+        f = open("file.txt", "w")
+        for element in t:
+            f.write("".join(map(str,element)))
+            f.write("\n")
+    else:
+        sys.exit(0)
 
-print("\n")
-# print("You got", len(t) ,"different combinations")
+def start():
+    intro()
+    main()
 
-ask_user = str(input("Do you want to safe all these combinations? (yes/no) : "))
-if ask_user == "yes" or "y":
-    f = open("file.txt", "w")
-    for element in t:
-        f.write("".join(map(str,element)))
-        f.write("\n")
-elif ask_user == "no" or "n":
-    sys.exit(0)
-else:
-    sys.exit(0)
-
+start()
