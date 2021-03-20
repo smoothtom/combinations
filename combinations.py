c5171b7144631a8a88ccee4cc1bb5784b366a1fe
smoothtom <t-lauth@web.de>
2021-03-20 16:26:22 +0100

    more changes, but for loop at the end still needs to be fixed

--- combinations.py
+++ combinations.py
@@ -5,17 +5,22 @@ import os
 import time 
 import datetime 
 
+# intro function 
 def intro():
     print("\n")
     print("*" * 41)
     print("** Welcome to my Combination Programm! **")
     print("*" * 41)
 
+# main function, where the main programm is written
 def main():
     print("\n")
     s = input("Please Enter something for different combinations: ")
     print("\n")
     t = list(itertools.permutations(s,len(s)))
+
+    # prints the length of the word from the user input
+    print("Your input is" ,len(s), "words long\n")
 
     print("loading the combinations...")
     time.sleep(2)
@@ -42,12 +47,19 @@ def main():
             print("".join(t[i]),"-->", x )
 
     print("\n")
-    i = input("Do you want to save all these combinations ? Please Enter yes or no: ")
-    if i == "yes":
+
+    # ask the user after the loop, to save the combinations in a extra .txt file
+    i = input("Do you want to save all these combinations ? Please Enter (y)es or (n)o: ")
+    if i == "yes" or "y":
+        print("\nYour combinations have been successfully saved in the file.txt")
         f = open("file.txt", "w")
         for element in t:
             f.write("".join(map(str,element)))
             f.write("\n")
+    elif i == "no" or "n":
+        print("You quit this programm, without saving all your combinations")
+        sys.exit(0)
+    # if user types no - then the programm will quit
     else:
         sys.exit(0)
 
